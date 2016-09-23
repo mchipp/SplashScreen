8a0829b50ae4076b9cc789edfe67dd5defa978e7
Mark Chipp <200180985@student.georgianc.on.ca>
2016-09-23 15:36:38 -0400

    Final commit

---
+++ SplashScreen/SplashScreenForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace SplashScreen
+{
+    partial class SplashScreenForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.SplashScreenTimer = new System.Windows.Forms.Timer(this.components);
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SplashScreenTimer
+            // 
+            this.SplashScreenTimer.Enabled = true;
+            this.SplashScreenTimer.Interval = 3000;
+            this.SplashScreenTimer.Tick += new System.EventHandler(this.SplashScreenTimer_Tick);
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.Font = new System.Drawing.Font("Corbel", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TitleLabel.Location = new System.Drawing.Point(75, 55);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(440, 59);
+            this.TitleLabel.TabIndex = 0;
+            this.TitleLabel.Text = "Mark\'s Splash Screen";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Corbel", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(129, 171);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(328, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Code that always hits the Mark!";
+            // 
+            // SplashScreenForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 262);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TitleLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "SplashScreenForm";
+            this.Opacity = 0.95D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Splash Screen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer SplashScreenTimer;
+        private System.Windows.Forms.Label TitleLabel;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SplashScreen/SplashScreenForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SplashScreen
+{
+    public partial class SplashScreenForm : Form
+    {
+        public SplashScreenForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SplashScreenTimer_Tick(object sender, EventArgs e)
+        {
+            SplashScreenTimer.Enabled = false;
+
+            // lines 26-27 can be uncommented when SplashScreenForm.cs is
+            // added to a project with a form to proceed to
+            //StartForm startForm = new StartForm();
+            //startForm.Show();
+            this.Hide();
+        }
+    }
+}
